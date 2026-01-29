057b296b2d4ffc9ad6738c0685e213808415f9d2
unknown <sirinethrasandy@gmail.com>
2026-01-29 12:20:35 +0530

    small change

--- src/identifiers/ObjectIdentifier.java
+++ src/identifiers/ObjectIdentifier.java
@@ -13,11 +13,11 @@ public class ObjectIdentifier {
 	s1.name="Madhu";
 	s1.phoneNumber=123;
 	s1.mailId="sirinethrasandy@gmail.com";
-//
-//	System.out.println(s1.name);
-//	System.out.println(s1.phoneNumber);
-//	System.out.println(s1.mailId); 
-	//Student s2= new Student();
+
+	System.out.println(s1.name);
+	System.out.println(s1.phoneNumber);
+	System.out.println(s1.mailId); 
+	Student s2= new Student();
 	s1.name="siri";
 	s1.phoneNumber=456;
 	s1.mailId="nethrasandy@gmail.com";
